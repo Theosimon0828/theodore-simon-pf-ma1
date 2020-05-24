40a4a7a050864b14044c753d16b08f584638cb83
Theosimon0828 <63924343+Theosimon0828@users.noreply.github.com>
2020-05-24 20:35:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// question 1
+var firstVar;
+// question 2
+var name = "Theodore";
+console.log(name);
+// question 3
+var number ="1";
+// question 4
+var division =20/5;
+console.log(division);
+// question 5
+typeof ("frog");
+// question 6
+var orderHasShipped = true;
+// question 7
+    if (orderHasShipped === true){
+        console.log("true");
+    } 
+    else {
+    console.log("false)");
+    }
+// question 8
+    for(var count = 1; count <= 9; count = count + 1) {
+        console.log(count);
+    }
